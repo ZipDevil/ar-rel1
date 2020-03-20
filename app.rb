5cdfdbaed00c61f7f99d5f7d38c2aa90b9164e49
Ubuntu <ubuntu@ip-172-31-85-204.ec2.internal>
2020-03-20 20:55:48 +0000

    add files in views folder, revisioned app.rb code.

--- app.rb
+++ app.rb
@@ -6,12 +6,52 @@ require './models/user'
 require './models/team'
 
 get '/' do
+    @users=User.all
+    erb :user_list
+end
+
+get '/' do
     User.all.to_yaml + Team.all.to_yaml
 end
 
+get '/new' do
+    erb :user_form
+end
+    
+get '/remove' do
+    erb :user_delete
+end
+
+# add the user returned by user_form and put up a confirmation message.
+# Here you would use the Active Record means of inserting a record.
+post '/create' do
+    # if User.exists?(params[:name])
+    # "User is already exist."
+    # else
+    # User.create!(name: params[:name], email: params[:email])
+    # "User #{params[:name]} created!"
+    # end
+    user = User.new
+    user.name = params[:name]
+    user.email = params[:email]
+    user.save 
+    "User #{params[:name]} created!"
+end    
+
+post '/delete' do
+    
+    user = User.find_by(name: params[:name])
+    if user
+        user.destroy
+        "User #{params[:name]} has been deleted."
+    else
+        "User not found."
+    end
+end
+
 get "/which_user" do
-  @users = User.all
-  erb :which_user
+    @users = User.all
+    erb :which_user
 end
 
 get '/users1' do
